15e4c55237bdc1b5a111abbf149b04d80144975e
Yue Yu <yueyu@utexas.edu>
2022-07-22 16:52:35 -0500

    Add files via upload

---
+++ main.m
@@ -0,0 +1,23 @@
+clear;clc;
+
+pa = struct; % structure var for storing all parameters
+pa.p = 3; % # of players
+pa.E = [ 1, -1,  0,  0,  1, -1;
+        -1,  1,  1, -1,  0,  0;
+         0,  0, -1,  1, -1,  1]; % incidence matrix
+  
+[pa.n, pa.m] = size(pa.E); % # of nodes and links in the graph 
+  
+
+pa.b = 0.1*ones(pa.p*pa.m, 1);
+pa.C = zeros(pa.p*pa.m, pa.p*pa.m);
+pa.lam = 0.01;
+pa.smat = [ 1,  0, -1;
+           -1,  1,  0;
+            0, -1   1];
+pa.s = pa.smat(:);
+%%
+
+[x, v, fval, output] = myfun(pa, zeros(pa.p*pa.m, 1), zeros(pa.p*pa.n, 1));
+reshape(x, [], pa.p)
+
---
+++ myfun.m
@@ -0,0 +1,16 @@
+function [x, v, fval, output] = myfun(pa, x0, v0)
+
+% x is the Nash defined by pa
+
+[equ, fval, ~, output] = fsolve(@nash, [x0; v0]);
+
+x = equ(1:pa.p*pa.m);
+v = equ(pa.p*pa.m+1:end);
+
+function y = nash(xi)
+
+y = [xi(1:pa.p*pa.m) - exp((1/pa.lam)*(kron(eye(pa.p), pa.E')*xi(pa.p*pa.m+1:end)-pa.b-pa.C*xi(1:pa.p*pa.m))-ones(pa.p*pa.m, 1));
+     pa.s - kron(eye(pa.p), pa.E)*xi(1:pa.p*pa.m)];
+end
+
+end
