e3387503505a48feccb1760d36c5d75d3ce3fba7
Vivian Chen <sc9by@virginia.edu>
2022-08-08 11:11:43 -0500

    fix node coordinates, add styling

--- plot_routing.jl
+++ plot_routing.jl
@@ -1,5 +1,11 @@
-using LightGraphs
+using Graphs
 using GraphRecipes, Plots
+
+# function graphToMatrix(g)
+#     im = Matrix(incidence_matrix(g))
+#     am = Matrix(adjacency_matrix(g))
+#     (;im=im, am=am)
+# end
 
 # create directed graph
 g = SimpleDiGraph(4)
@@ -10,21 +16,16 @@ add_edge!(g, 3, 4)
 add_edge!(g, 2, 3)
 add_edge!(g, 3, 2)
 
-function graphToMatrix(g)
-    im = Matrix(incidence_matrix(g))
-    am = Matrix(adjacency_matrix(g))
-    (;im=im, am=am)
-end
-
 # create incidence matrix from directed graph for computation
 im = Matrix(incidence_matrix(g))
-# E = [1 1 0 0 0 0; -1 0 1 0 1 -1; 0 -1 0 1 -1 1; 0 0 -1 -1 0 0]
-E = -im
-@show E
-
-# create adjacency matrix from directed graph for visualization
-am = Matrix(adjacency_matrix(g))
+E = -im; @show E
 
 # plot directed graph
-graphplot(am, names=1:4)
-savefig("routing_plot.png")
+δ=0.01
+locs_x = [1, 2-δ, 2+δ, 3]
+locs_y = [2, 1, 3, 2]
+edgelabel_dict = Dict((1,2) => "1", (1,3) => "2", (2,4) => "3", (3,4) => "4", (2,3) => "5", (3,2) => "6")
+graphplot(g, fontsize=11,
+          names=1:nv(g), x=locs_x, y=locs_y, nodeshape=:circle, nodesize=0.4,
+          edgelabel=edgelabel_dict, curvature_scalar=0.1)
+savefig("routing_plot_coordinated.png")
