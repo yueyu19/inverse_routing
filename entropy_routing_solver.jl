c734d020c7bba7eb40442072fba5c863bc5cd82b
Vivian Chen <sc9by@virginia.edu>
2022-08-15 15:28:48 -0500

    use autodiff in forward solver, added optional jacobian function

--- entropy_routing_solver.jl
+++ entropy_routing_solver.jl
@@ -17,7 +17,7 @@ Returns:
 - v: value vector/function associated to all nodes
 """
 function solve_entropy_routing(pa, λ)
-
+    println("calling solve_entropy_routing...")
     function nash!(F, xi)
         # x = xi[1:pa.p*pa.m]
         # v = xi[pa.p*pa.m+1:end]
@@ -32,10 +32,36 @@ function solve_entropy_routing(pa, λ)
         end
     end
 
+    function j!(H, xi)
+        # x = xi[1:pa.p*pa.m]
+        # v = xi[pa.p*pa.m+1:end]
+
+        # compute D, J 
+        D = diagm(vec(exp.(1/λ * (kron(I(pa.p), pa.E')*xi[pa.p*pa.m+1:end] - pa.b - pa.C*xi[1:pa.p*pa.m]) - ones(pa.p*pa.m, 1)))) # dim: 18x18
+        
+        # for i in 1:pa.p*pa.m, j in 1:pa.p*pa.m
+        #     J[i, j] = I(pa.p*pa.m)[i,j] + (1/λ*D*pa.C)[i,j]
+        # end
+        # for i in 1:pa.p*pa.m, j in pa.p*pa.m+1:pa.p(pa.m+pa.n)
+        #     J[i, j] = 1/λ*D*kron(I(pa.p), pa.E')[i,j]
+        # end
+        # for i in pa.p*pa.m+1:pa.p(pa.m+pa.n), j in 1:pa.p*pa.m
+        #     J[i, j] = kron(-I(pa.p), pa.E)[i,j]
+        # end
+        # for i in pa.p*pa.m+1:pa.p(pa.m+pa.n), j in pa.p*pa.m+1:pa.p(pa.m+pa.n)
+        #     J[i, j] = 0
+        # end
+
+        J = [I(pa.p*pa.m)+1/λ*D*pa.C 1/λ*D*kron(I(pa.p), pa.E'); kron(-I(pa.p), pa.E) zeros(pa.p*pa.n, pa.p*pa.n)] # dim: 27x27
+        for i in 1:pa.p*(pa.m+pa.n), j in 1:pa.p*(pa.m+pa.n)
+            H[i, j] = J[i, j]
+        end
+    end
+
     x0 = 0.5.*ones(pa.p*pa.m, 1)
     v0 = 0.5.*ones(pa.p*pa.n, 1)
-    sol = nlsolve(nash!, [x0; v0])
-    # print(sol)
+    sol = nlsolve(nash!, [x0; v0], autodiff = :forward, show_trace=false, ftol=1e-4)
+    # sol = nlsolve(nash!, j!, [x0; v0], show_trace=true)
 
     (;x = sol.zero[1:pa.p*pa.m],
       v = sol.zero[pa.p*pa.m+1:end])
@@ -45,20 +71,20 @@ end
 """
 creating atomic routing game instance, pa
 """
-function pa()
-    p = 3
-    E = [1 -1  0  0  1 -1; -1  1  1 -1  0  0; 0  0 -1  1 -1 1]
-    n, m = size(E)
-    s = vec([1 0 -1; -1 1 0; 0 -1 1])
-    b = 0.1 * ones(p*m, 1)
-    C = zeros(p*m, p*m)
-    (; p=p, E=E, n=n, m=m, s=s, b=b, C=C)
-end
+# function pa()
+#     p = 3
+#     E = [1 -1  0  0  1 -1; -1  1  1 -1  0  0; 0  0 -1  1 -1 1]
+#     n, m = size(E)
+#     s = vec([1 0 -1; -1 1 0; 0 -1 1])
+#     b = 0.1 * ones(p*m, 1)
+#     C = zeros(p*m, p*m)
+#     (; p=p, E=E, n=n, m=m, s=s, b=b, C=C)
+# end
 
 
 """
 calling solve_entropy_routing()
 """
-x, v = solve_entropy_routing(pa(), 0.01)
+# x, v = solve_entropy_routing(pa(), 0.01)
 # @show x
 # @show v
