b058e17aeff70b06d8aa599291fcc29135a90c41
Shenghui Chen <schen@asg-a14249.oden.utexas.edu>
2022-08-17 11:22:44 -0500

    add cmd line arg parsing

--- plotting_rho.jl
+++ plotting_rho.jl
@@ -1,16 +1,23 @@
 using JLD2
 using Plots
 
-rho_list = Float64[]
-psi_end_list = Float64[]
+game_name = "grid_graph3"
 
-for ρ in [1.0, 2.0, 5.0, 10.0]
-    @load "results/grid_graph3_λ=(0.01)_α=(0.01)_ϵ=(0.01)_ρ=($ρ)/output.jld2" ψ_vals
-    # println("ρ=$ρ", "   ", "ψ_vals[end]=$(ψ_vals[end])")
-    push!(rho_list, ρ)
+# parse rho_list from cmd line arg
+rho_list = let expr = Meta.parse(ARGS[1])
+    @assert expr.head == :vect
+    Float64.(expr.args)
+end
+
+# collect psi_end_list
+psi_end_list = Float64[]
+for ρ in rho_list
+    @load "results/$(game_name)_λ=(0.01)_α=(0.01)_ϵ=(0.01)_ρ=($ρ)/output.jld2" ψ_vals
+    println("ρ=$ρ", "   ", "ψ_vals[end]=$(ψ_vals[end])")
     push!(psi_end_list, ψ_vals[end])
 end
 
-println("saving rho paramter search plot in results/")
+# plot and save rho graph in results/
+println("saving rho paramter search plot in 'results/$(game_name)_rho_parameter_search.png'")
 plot(rho_list, psi_end_list, xlabel="ρ", ylabel="ψ(x) convergence value")
-savefig("results/grid_graph3_rho_parameter_search.png")
+savefig("results/$(game_name)_rho_parameter_search.png")
